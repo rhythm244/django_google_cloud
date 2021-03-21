9934c783ece80349c3d79b846231b12c22ecc090
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2021-03-21 12:31:43 +0700

    gcloud: style css color 21/03/2021

--- static/myapp/script.js
+++ static/myapp/script.js
@@ -1,16 +1,17 @@
-function hello() {
-    if (document.querySelector("#joinworld").innerHTML === '601 SQDN !!!!!!!')
-    { 
-        document.querySelector("#joinworld").innerHTML = 'สวัสดีครับ เว็บแอปสำหรับฝูง เพิ่งทำการจัดทำ มีอะไรแนะนำบอกได้ครับ ขอบคุณครับ'; 
-    }
-    else 
-    {
-        document.querySelector("#joinworld").innerHTML = '601 SQDN !!!!!!!';
-    }
-}
+// function hello() {
+//     if (document.querySelector("#joinworld").innerHTML === '601 SQDN !!!!!!!')
+//     { 
+//         document.querySelector("#joinworld").innerHTML = 'สวัสดีครับ เว็บแอปสำหรับฝูง เพิ่งทำการจัดทำ มีอะไรแนะนำบอกได้ครับ ขอบคุณครับ'; 
+//     }
+//     else 
+//     {
+//         document.querySelector("#joinworld").innerHTML = '601 SQDN !!!!!!!';
+//     }
+// }
 
-//เมื่อเกิด event DOMcontentLoaded ก็คือเมื่อ เพจโหลดเสร็จ ให้รัน funtion hello เมื่อมีการคลิกที่ id ชื่อว่า hello_button
-document.addEventListener('DOMContentLoaded', function() {
-document.querySelector("#hello_button").onclick = hello;
-});
+// //เมื่อเกิด event DOMcontentLoaded ก็คือเมื่อ เพจโหลดเสร็จ ให้รัน funtion hello เมื่อมีการคลิกที่ id ชื่อว่า hello_button
+// document.addEventListener('DOMContentLoaded', function() {
+// document.querySelector("#hello_button").onclick = hello;
+// });
 
+// <button class ="btn btn-secondary" id=hello_button>Hello</button>
